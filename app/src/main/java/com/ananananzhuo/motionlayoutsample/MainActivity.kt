d988b2036fa44896e8f7a49ac2411ab87bfaf990
大师兄 <1275054668@qq.com>
2021-08-26 06:25:35 +0800

    firstcommit

---
+++ app/src/main/java/com/ananananzhuo/motionlayoutsample/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.ananananzhuo.motionlayoutsample
+
+import android.content.Intent
+import com.ananananzhuo.mvvm.activity.CustomAdapterActivity
+import com.ananananzhuo.mvvm.bean.bean.ItemData
+import com.ananananzhuo.mvvm.callback.CallData
+import com.ananananzhuo.mvvm.callback.Callback
+
+class MainActivity : CustomAdapterActivity() {
+
+    override fun getAdapterDatas(): MutableList<ItemData> = mutableListOf(
+        ItemData(title = "写三个布局实现简单滑动",callback = object :Callback{
+            override fun callback(callData: CallData) {
+                toAnimPage(R.layout.activity_simple_anim)
+            }
+        }),
+        ItemData(title = "写一个布局实现简单滑动",callback = object :Callback{
+            override fun callback(callData: CallData) {
+                toAnimPage(R.layout.anim_0)
+            }
+        }),
+    )
+
+    private fun toAnimPage(layoutId: Int) {
+        startActivity(Intent(this,ShowAnimActivity::class.java).apply {
+            putExtra("layoutId",layoutId)
+        })
+    }
+
+    override fun showFirstItem(): Boolean =true
+
+}
---
+++ app/src/main/java/com/ananananzhuo/motionlayoutsample/ShowAnimActivity.kt
@@ -0,0 +1,12 @@
+package com.ananananzhuo.motionlayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowAnimActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val layoutId = intent.getIntExtra("layoutId", R.layout.activity_simple_anim_start)
+        setContentView(layoutId)
+    }
+}
