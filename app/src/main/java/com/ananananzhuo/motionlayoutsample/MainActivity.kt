1fe831445a9d0d9ef2d05eb5747556c64ca4c767
大师兄 <1275054668@qq.com>
2021-08-28 18:11:37 +0800

    MotionLayout配合layoutbehavior使用

--- app/src/main/java/com/ananananzhuo/motionlayoutsample/MainActivity.kt
+++ app/src/main/java/com/ananananzhuo/motionlayoutsample/MainActivity.kt
@@ -10,44 +10,56 @@ import com.ananananzhuo.mvvm.callback.Callback
 class MainActivity : CustomAdapterActivity() {
 
     override fun getAdapterDatas(): MutableList<ItemData> = mutableListOf(
-        ItemData(title = "写三个布局实现简单滑动",callback = object :Callback{
+        ItemData(title = "写三个布局实现简单滑动", callback = object : Callback {
             override fun callback(callData: CallData) {
                 toAnimPage(R.layout.activity_simple_anim)
             }
         }),
-        ItemData(title = "写一个布局实现简单滑动",callback = object :Callback{
+        ItemData(title = "写一个布局实现简单滑动", callback = object : Callback {
             override fun callback(callData: CallData) {
                 toAnimPage(R.layout.anim_0)
             }
         }),
-        ItemData(title = "实现底部向上滑动title，变旋转标题",callback = object :Callback{
+        ItemData(title = "实现底部向上滑动title，变旋转标题", callback = object : Callback {
             override fun callback(callData: CallData) {
                 toAnimPage(R.layout.anim_1)
             }
         }),
-        ItemData(title = "（本例暂时未完成）滑动NestScrollView，topbar向上滑动",callback = object :Callback{
+        ItemData(title = "（本例暂时未完成）滑动NestScrollView，topbar向上滑动", callback = object : Callback {
             override fun callback(callData: CallData) {
                 toAnimPage(R.layout.anim_2)
             }
         }),
-        ItemData(title = "实现keyframe动画",callback = object :Callback{
+        ItemData(title = "实现keyframe动画", callback = object : Callback {
             override fun callback(callData: CallData) {
                 toAnimPage(R.layout.anim_3)
             }
         }),
-        ItemData(title = "OnSwip.dragThreshold",callback = object :Callback{
+        ItemData(title = "OnSwip.touchRegionId", callback = object : Callback {
             override fun callback(callData: CallData) {
                 toAnimPage(R.layout.anim_field0)
             }
         }),
-    )
+        ItemData(title = "OnSwip.touchAnchorSide", callback = object : Callback {
+            override fun callback(callData: CallData) {
+                toAnimPage(R.layout.anim_field1)
+            }
+        }),
+        ItemData(title = "实现嵌套滑动的效果", callback = object : Callback {
+            override fun callback(callData: CallData) {
+                startActivity(Intent(this@MainActivity, NestScrollActivity::class.java))
+            }
+        }),
+
+
+        )
 
     private fun toAnimPage(layoutId: Int) {
-        startActivity(Intent(this,ShowAnimActivity::class.java).apply {
-            putExtra("layoutId",layoutId)
+        startActivity(Intent(this, ShowAnimActivity::class.java).apply {
+            putExtra("layoutId", layoutId)
         })
     }
 
-    override fun showFirstItem(): Boolean =true
+    override fun showFirstItem(): Boolean = true
 
 }
---
+++ app/src/main/java/com/ananananzhuo/motionlayoutsample/NestScrollActivity.kt
@@ -0,0 +1,11 @@
+package com.ananananzhuo.motionlayoutsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NestScrollActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nest_scroll)
+    }
+}
